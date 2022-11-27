05410c1dee188e79e62bc2d9d9b0ed60dfc251e9
DimitriiTrater <dima1603040@gmail.com>
2022-11-27 11:28:24 +0200

    Add Sorting dir

--- include.h
+++ include.h
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include <vector>
-#include "Bubble Sort/bubble_sort.h"
-#include "Bucket Sort/bucket_sort.h"
-#include "Insertion Sort/insertion_sort.h"
-#include "Standard Tools/std_tools.h"
-#include "Quick Sort/quick_sort.h"
-#include "Selection Sort/selection_sort.h"
-#include "Bogosort/bogosort.h" 
-#include "Merge Sort/merge_sort.h" 
 
+#include "Standard Tools/std_tools.h"
+
+#include "Sorting/Bubble Sort/bubble_sort.h"
+#include "Sorting/Bucket Sort/bucket_sort.h"
+#include "Sorting/Insertion Sort/insertion_sort.h"
+#include "Sorting/Quick Sort/quick_sort.h"
+#include "Sorting/Selection Sort/selection_sort.h"
+#include "Sorting/Merge Sort/merge_sort.h" 
+#include "Sorting/Bogosort/bogosort.h" 
